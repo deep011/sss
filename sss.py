c65bad1ab88b9bdfd2c5a38e7a1e789588e1eaa6
deep011 <diguo58@gmail.com>
2017-09-25 11:40:02 +0800

    support 'del_h' and 'del_m' columns of 'command_detail' section for memcached

--- sss.py
+++ sss.py
@@ -2373,6 +2373,8 @@ StatusColumn("flu", "flush_all", 0, column_flags_speed, field_handler_common, ["
 memcached_command_detail_section = StatusSection("command_detail", "",[
 StatusColumn("get_h", "get_hits", 0, column_flags_speed, field_handler_common, ["get_hits"], "Number of retrieval commands hits per second. Retrieval commands include get, gets and so on."),
 StatusColumn("get_m", "get_misses", 0, column_flags_speed, field_handler_common, ["get_misses"], "Number of retrieval commands misses per second. Retrieval commands include get, gets and so on."),
+StatusColumn("del_h", "delete_hits", 0, column_flags_speed, field_handler_common, ["delete_hits"], "Number of delete commands hits per second."),
+StatusColumn("del_m", "delete_misses", 0, column_flags_speed, field_handler_common, ["delete_misses"], "Number of delete commands misses per second.")
 ], [get_memcached_status],[ALL_COLUMNS],
 "memcached command detail status, collect from \'stats\'")
 
