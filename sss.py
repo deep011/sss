ad12403ad62c1edb7078281491c7a2ed474d69a3
deep011 <diguo58@gmail.com>
2017-10-12 15:17:47 +0800

    support 'CMPs' columns of 'command' section for mysql

--- sss.py
+++ sss.py
@@ -1761,7 +1761,8 @@ StatusColumn("ATPs", "alter_table_per_second", 0, column_flags_speed, field_hand
 StatusColumn("TrPs", "truncate_per_second", 0, column_flags_speed, field_handler_common,["Com_truncate"], "Truncate commands per second. Collected from 'Com_truncate'."),
 StatusColumn("StartSPs", "start_slave_per_second", 0, column_flags_speed, field_handler_common,["Com_start_slave"], "Start slave commands per second. Collected from 'Com_start_slave'."),
 StatusColumn("StopSPs", "stop_slave_per_second", 0, column_flags_speed, field_handler_common,["Com_stop_slave"], "Stop slave commands per second. Collected from 'Com_stop_slave'."),
-StatusColumn("KPs", "kill_per_second", 0, column_flags_speed, field_handler_common,["Com_kill"], "Kill commands per second. Collected from 'Com_kill'.")
+StatusColumn("KPs", "kill_per_second", 0, column_flags_speed, field_handler_common,["Com_kill"], "Kill commands per second. Collected from 'Com_kill'."),
+StatusColumn("CMPs", "change_master_per_second", 0, column_flags_speed, field_handler_common,["Com_change_master"], "Change master commands per second. Collected from 'Com_change_master'.")
 ], [get_mysql_status],["TPs","QPs","DPs","IPs","UPs","DIUPs"],
 "mysql commands status, collect from \'show global status\'")
 
